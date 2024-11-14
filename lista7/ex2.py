a9f975a5467937bb77ac717833edc23a34ea5e2a
Igor-Roberto-Alves <al.igor.alves@impatech.org.br>
2024-11-13 23:46:48 -0300

    Atualization Lista 7 :)

--- lista7/ex2.py
+++ lista7/ex2.py
@@ -1,4 +1,5 @@
-import random
+def dist(p1, p2):
+    return ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** 1 / 2
 
 
 class vert:
@@ -13,108 +14,81 @@ class vert:
         return self.name
 
 
-class grafo:
-    def __init__(self, list_vert):
-        self.lv = list_vert
-        self.edge = {vert: [] for vert in self.lv}
+class edge:
+    def __init__(self, p1, p2, valor):
+        self.p1 = p1
+        self.p2 = p2
+        self.valor = valor
+
+    def __repr__(self):
+        return f"-> {self.p2}: {self.valor}"
+
+
+# Escolhi aqui definir um grafo por matriz, achei mais simples para tratar arestas com peso
+class grafo_matriz:
+    def __init__(self, matriz):
+        self.dict = {}
+        self.list_vert = []
+        for i in range(len(matriz)):
+            vertex = vert(f"{i}")
+            self.list_vert.append(vertex)
+            self.dict[vertex] = []
+
+        # A matriz é sempre quadrada
+        for i in range(len(matriz)):
+            for j in range(len(matriz)):
+                if i != j:
+                    if matriz[i][j] != 0:
+                        self.dict[self.list_vert[i]].append(edge(i, j, matriz[i][j]))
 
     def __str__(self):
         stri = ""
-        for vert in self.edge.keys():
+        for vert in self.dict.keys():
             stri += vert.name + ":"
-            stri += " " + "".join(str([conex for conex in self.edge[vert]]))
+            stri += " " + "".join(str([conex for conex in self.dict[vert]]))
             stri += "\n"
         return stri
 
-    def adjascent(self, x, y):
-        if y in self.edge[x]:
-            return True
-        if x in self.edge[y]:
-            return True
-        return False
 
-    def neighbors(self, x):
-        lista_v = []
-        for i in self.edge[x]:
-            lista_v.append(i)
-        return lista_v
+def minnimun_spanning_tree(points):
+    matriz = []
+    for point in points:
+        row = []
+        for point2 in points:
+            if point2 == point:
+                row.append(0)
+            else:
+                row.append(dist(point, point2))
 
-    def add_vertex(self, x):
-        if x in self.lv:
-            return False
-        else:
-            self.edge[x] = []
+        # Definindo a matriz com o peso referente a distância euclidiana
+        matriz.append(row)
 
-            return True
+    num_vert = len(matriz)
+    minimal_matriz = [[0 for _ in range(num_vert)] for _ in range(num_vert)]
+    priorityqueue = []
+    visiteds = set()
+    # Começaremos com o vértice 0, assim adicionado ele nos visitados
+    visiteds.add(0)
+    while len(visiteds) < num_vert:  # Enquanto não estão todos ligados
+        for i in range(num_vert):
+            for j in range(num_vert):
+                if i != j and j not in visiteds:  # Se j ainda não foi incluído no grafo
+                    priorityqueue.append(
+                        (i, j, matriz[i][j])
+                    )  # Adicionamos ele na fila de prioridade
+            if priorityqueue:
+                priorityqueue.sort(
+                    key=lambda x: x[2]
+                )  # Ordenando a fila de prioridade pela distância de i <-> j
+                position1, position2, value = priorityqueue.pop(0)
+                minimal_matriz[position1][position2] = value
+                visiteds.add(
+                    position2
+                )  # Como foi adicionado uma aresta para j, precisamos contar j como visitado
 
-    def remove_vertex(self, x):
-        if x in self.edge:
-            del self.edge[x]
-
-            for vert in self.edge.keys():
-                if x in self.edge[vert]:
-                    self.edge[vert].remove(x)
-            return True
-        return False
-
-    def add_edge(self, x, y):
-        if y in self.edge[x]:
-            return False
-        else:
-            self.edge[x].append(y)
-            return True
-
-    def remove_edge(self, x, y):
-        if y in self.edge[x]:
-            self.edge[x].remove(y)
-            return True
-        else:
-            return False
-
-    @staticmethod
-    def get_vertex_value(x):
-        return x.valor
-
-    @staticmethod
-    def set_vertex_value(x, new_valor):
-        x.valor = new_valor
+    return grafo_matriz(minimal_matriz)
 
 
-def dist(point1, point2):
-    x1, y1 = point1[0], point1[1]
-    x2, y2 = point2[0], point2[1]
-    return ((x2 - x1) ^ 2 + (y2 - y1) ^ 2) ** 1 / 2
+point = ((1, 0), (0, 1))
 
-
-def minnimun_tree(points):
-    Graf = grafo([])
-    for i in range(len(points)):
-        Graf.add_vertex(vert(f"{i+1}", points[i]))
-    print(Graf)
-    for verta in Graf.edge.keys():
-        print(verta.valor)
-
-    for i in Graf.edge.keys():
-        min_vertex = float("inf")
-        vertix = None
-        for j in Graf.edge.keys():
-            if dist(i.valor, j.valor) < min_vertex:
-                min_vertex = dist(i.valor, j.valor)
-                vertix = j
-
-        Graf.edge[i] = vertix
-
-def menor_ligacao(points):
-    pass
-def divide_conquis(points):
-    points.sort()
-    if len(points) <= 3:
-        menor_ligacao(points)
-    else:
-        divide_conquis(points[0:len(points)//2])
-        divide_conquis(points[len(points)//2:])
-        
-
-
-points = [[0, 0], [0, 1], [1, 0], [3, 0], [4, 0], [9, 3]]
-minnimun_tree(points)
+print(minnimun_spanning_tree(point))
